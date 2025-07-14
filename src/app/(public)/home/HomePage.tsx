90932e2e4fa4682462ef47ab3edcb38d38a415b6
bongshim <bongshime@gmail.com>
2025-07-14 16:21:59 +0100

    Refactor HTML structure and improve code consistency in index.html; update import statements and formatting in HomePage.tsx, hero.tsx, and scrollablecards.tsx for better readability and maintainability.

--- src/app/(public)/home/HomePage.tsx
+++ src/app/(public)/home/HomePage.tsx
@@ -1,30 +1,29 @@
-import { useNavigate } from "react-router"
-import Section from "./components/Section"
-import Navbar from "./components/navbar"
-import HeroSection from "./components/hero"
-import DashedLine from "./components/dashlines"
-import WelcomeSection from "./components/welcomesection"
-import LatestInformation from "./components/latestinformation"
-import ScrollableCards from "./components/scroolablecards"
-import DocumentSection from "./components/documentsection"
-import BppApplications from "./components/bppapplications"
-import Footer from "./components/footer"
+import { useNavigate } from 'react-router';
+import Navbar from './components/navbar';
+import HeroSection from './components/hero';
+import DashedLine from './components/dashlines';
+import WelcomeSection from './components/welcomesection';
+import LatestInformation from './components/latestinformation';
+import ScrollableCards from './components/scroolablecards';
+import DocumentSection from './components/documentsection';
+import BppApplications from './components/bppapplications';
+import Footer from './components/footer';
 
-const Homepage = () =>{
-    const navigate = useNavigate()
-    return(
-        <>
-  <Navbar  />
-  <HeroSection />
-  <DashedLine />
-  <WelcomeSection />
-  <LatestInformation />
-  <ScrollableCards/>
-  <DocumentSection/>
-  <BppApplications/>
-  <Footer/>
-        </>
-    )
-}
+const Homepage = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <DashedLine />
+      <WelcomeSection />
+      <LatestInformation />
+      <ScrollableCards />
+      <DocumentSection />
+      <BppApplications />
+      <Footer />
+    </>
+  );
+};
 
-export default Homepage
+export default Homepage;
--- src/app/(public)/home/components/scroolablecards.tsx
+++ src/app/(public)/home/components/scroolablecards.tsx
@@ -11,28 +11,28 @@ const cardData = [
     highlight: 'Petition Submissions',
     subtitle: 'are currently online?',
     linkText: 'Submit Now',
-    bgImage: '/assets/images/frame1.png',
+    bgImage: '/assets/images/Frame1.png',
   },
   {
     title: 'Are you a',
     highlight: 'Federal Government ?',
     subtitle: 'Contractor, Consultant or service provider',
     linkText: 'Register with Ease',
-    bgImage: '/assets/images/frame2.png',
+    bgImage: '/assets/images/Frame2.png',
   },
   {
     title: 'Report any',
     highlight: 'suspicious procurement activity',
     subtitle: 'and get full backings of the bureau',
     linkText: 'Report here',
-    bgImage: '/assets/images/frame1.png',
+    bgImage: '/assets/images/Frame1.png',
   },
   {
     title: 'Are you a',
     highlight: 'Procurement Officer?',
     subtitle: 'Update your profile now',
     linkText: 'Update your profile now',
-    bgImage: '/assets/images/frame2.png',
+    bgImage: '/assets/images/Frame2.png',
   },
 ];
 
@@ -49,31 +49,31 @@ export default function ScrollableCards() {
   };
 
   return (
-    <div className="w-full pt-10 pb-16 px-4 flex items-center gap-4 relative bg-white plus">
+    <div className='w-full pt-10 pb-16 px-4 flex items-center gap-4 relative bg-white plus'>
       {/* Left Scroll Button */}
       <button
         onClick={() => scroll('left')}
-        className="hidden md:flex items-center justify-center w-10 h-10  hover:bg-gray-300 z-10"
+        className='hidden md:flex items-center justify-center w-10 h-10  hover:bg-gray-300 z-10'
       >
-        <ArrowBackIosNewIcon fontSize="small" />
+        <ArrowBackIosNewIcon fontSize='small' />
       </button>
 
       {/* Scrollable Container */}
       <div
         ref={scrollRef}
-        className="flex gap-4 overflow-x-auto scroll-smooth no-scrollbar md:w-full"
+        className='flex gap-4 overflow-x-auto scroll-smooth no-scrollbar md:w-full'
       >
         {cardData.map((card, index) => (
           <div
             key={index}
-            className="text-white rounded-xl w-[90%] sm:w-[300px] md:w-[30%] shrink-0 p-6 flex flex-col justify-between shadow-lg bg-cover bg-center bg-no-repeat relative plus"
+            className='text-white rounded-xl w-[90%] sm:w-[300px] md:w-[30%] shrink-0 p-6 flex flex-col justify-between shadow-lg bg-cover bg-center bg-no-repeat relative plus'
             style={{ backgroundImage: `url(${card.bgImage})` }}
           >
             {/* Optional overlay for readability */}
             {/* <div className="absolute inset-0 bg-black/60 rounded-xl"></div> */}
 
-            <div className="relative z-10">
-              <h3 className="text-[16px] font-semibold md:text-[24px] leading-[120%] md:leading-[100%]">
+            <div className='relative z-10'>
+              <h3 className='text-[16px] font-semibold md:text-[24px] leading-[120%] md:leading-[100%]'>
                 {card.title}
               </h3>
               <p
@@ -83,14 +83,16 @@ export default function ScrollableCards() {
               >
                 {card.highlight}
               </p>
-              <p className="text-[16px] md:text-[24px] leading-[100%]">
+              <p className='text-[16px] md:text-[24px] leading-[100%]'>
                 {card.subtitle}
               </p>
             </div>
-            <div className="relative z-10 mt-6 flex items-center justify-between">
-              <p className="text-[14px] font-[400] underline ">{card.linkText}</p>
-              <div className="bg-white rounded-full p-2 text-black">
-                <NorthEastIcon fontSize="small" />
+            <div className='relative z-10 mt-6 flex items-center justify-between'>
+              <p className='text-[14px] font-[400] underline '>
+                {card.linkText}
+              </p>
+              <div className='bg-white rounded-full p-2 text-black'>
+                <NorthEastIcon fontSize='small' />
               </div>
             </div>
           </div>
@@ -100,9 +102,9 @@ export default function ScrollableCards() {
       {/* Right Scroll Button */}
       <button
         onClick={() => scroll('right')}
-        className="hidden md:flex items-center justify-center w-10 h-10  hover:bg-gray-300 z-10"
+        className='hidden md:flex items-center justify-center w-10 h-10  hover:bg-gray-300 z-10'
       >
-        <ArrowForwardIosIcon fontSize="small" />
+        <ArrowForwardIosIcon fontSize='small' />
       </button>
     </div>
   );
